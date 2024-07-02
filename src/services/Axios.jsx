dc36709c015c3a9c2af5d5fde0145885956c784b
CrisOos <cristian.oostdijk@avenida.com>
2024-07-01 21:26:04 -0300

    fix for connection

--- src/services/Axios.jsx
+++ src/services/Axios.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const apiUrl = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000'
+
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: apiUrl,
 });
 
 export default axiosInstance;
