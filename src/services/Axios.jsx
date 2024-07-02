60a083d82e6c1d3de532a02d7fee81a1fc3575a8
CrisOos <cristian.oostdijk@avenida.com>
2024-07-01 23:32:48 -0300

    axios input

--- src/services/Axios.jsx
+++ src/services/Axios.jsx
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = process.env.NODE_ENV === 'production'
-  ? 'https://vacations-back-1jco.onrender.com'
-  : 'http://localhost:3000';
+const apiUrl = 'http://localhost:3000';
 
 const axiosInstance = axios.create({
   baseURL: apiUrl,
